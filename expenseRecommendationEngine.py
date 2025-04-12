6496bf1e7d51b481fe56fb403904c5a02f3ca04e
ugwu_kingsley <kingsleyikenna2019@gmail.com>
2025-04-12 06:38:15 +0300

    modified:   expenseRecommendationEngine.py

--- expenseRecommendationEngine.py
+++ expenseRecommendationEngine.py
@@ -9,6 +9,7 @@ categories = {
     "unavailable" : defaultdict(list)
 }
 
+
 #provide the period
 print("Provide the time frame of your current expense (as):\n" \
 "[0] = daily\n" \
@@ -19,6 +20,20 @@ while True:
     period = input("period = ")
     try:
         period = int(period)
+        break
     except ValueError:
         print("Enter only the numbers 0,1,2")
 
+def display_options():
+    print("\nThese are the available options for you to choose from. Use the index numbers 0,1,... to select an option.")
+    index = 0
+    for option in categories:
+        if index == 5:
+            break
+        print("[", str(index), "] = ", option)
+        index = index + 1
+
+# def get_daily_expense():
+#     expenseDict = {}
+
+display_options()

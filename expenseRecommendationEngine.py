921afef5b69e329c984fe59985f8d6c22ba1eb05
ugwu_kingsley <kingsleyikenna2019@gmail.com>
2025-04-12 06:19:57 +0300

    expenseRecommendationEngine.py

---
+++ expenseRecommendationEngine.py
@@ -0,0 +1,12 @@
+from collections import defaultdict
+
+categories = {
+    "food" : defaultdict(list),
+    "transport" : defaultdict(list),
+    "books" : defaultdict(list),
+    "bills" : defaultdict(list),
+    "entertaiment" : defaultdict(list),
+    "unavailable" : defaultdict(list)
+}
+
+#provide the 

a2f8d2995f523839c76b5d0a6e8ac5aadcf2fd7f
ugwu_kingsley <kingsleyikenna2019@gmail.com>
2025-04-12 06:26:25 +0300

    modified:   expenseRecommendationEngine.py

--- expenseRecommendationEngine.py
+++ expenseRecommendationEngine.py
@@ -11,9 +11,14 @@ categories = {
 
 #provide the period
 print("Provide the time frame of your current expense (as):\n" \
-"[0] = daily" \
-"[1] = weekly" \
-"[2] = monthly")
+"[0] = daily\n" \
+"[1] = weekly\n" \
+"[2] = monthly\n")
 
 while True:
     period = input("period = ")
+    try:
+        period = int(period)
+    except ValueError:
+        print("Enter only the numbers 0,1,2")
+
